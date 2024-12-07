bdac579cd1fb98fb0e397933d0ab163bb3ac125e
WillowSh <jjsssss123@163.com>
2024-12-07 17:45:25 +0800

    v2

--- 1-Computer_Graphics/167HW01/code/src/hw3.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <vector>
-#include <string>
-
-void hw_3_1(const std::vector<std::string> &params);
-void hw_3_2(const std::vector<std::string> &params);
-void hw_3_3(const std::vector<std::string> &params);
-void hw_3_4(const std::vector<std::string> &params);
